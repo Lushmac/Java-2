4471a0645d919e6570accba4b2cbadfb100117f6
Lushmac <luckysaleh@gmail.com>
2024-03-24 14:57:12 +0100

    created an app on how to delete student names or a string

---
+++ branch-1/java.js
@@ -0,0 +1,25 @@
+const DeleteButton = document.getElementById("button");
+const studentContainer = document.getElementById("resourceSection");
+const studentCard = studentContainer.querySelectorAll(".resource");
+let studentObjectArray = [...studentCard];
+
+console.log(studentObjectArray);
+DeleteButton.addEventListener("click", function () {
+  setTimeout(function () {
+    let index = Number(
+      prompt(
+        `Enter the Index of the Student you want to delete from 0 to ${
+          studentObjectArray.length - 1
+        }:`
+      )
+    );
+    if (!isNaN(index) && index >= 0 && index <= studentObjectArray.length) {
+      studentObjectArray.splice(index, 1);
+      studentContainer.removeChild(studentContainer.children[index]);
+    } else {
+      alert("Check the number you entered and try again");
+    }
+  }, 500);
+});
+
+
